dd27a1eab9285fb17a663724b36cb82b9a4ff218
suyeshb@gmail.com <suyeshb@gmail.com>
2013-08-30 01:02:14 -0400

    fake facebook

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Treebook::Application.config.secret_key_base = '8891116a9a66706d223f6e357bf1edf6b3286c819a2a8be91befa62b41a6e1c3bc5ed4017fbd8f40111026512d5348a7fbae2b0b498d83889bf79f05604395a0'
